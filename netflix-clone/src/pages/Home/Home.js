0c21fd650a30a18ab12877a52221d061b2509209
BrianLigh <brianlight2002@yahoo.com>
2024-05-06 20:06:38 -0700

    Row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/banner/Banner'
+import RowList from '../../components/Rows/RowList/RowList'
 
 const Home = () => {
   return (
     <>
         <Header/>
         <Banner/>
+        <RowList/>
         <Footer/>
+
     </>
   )
 }
